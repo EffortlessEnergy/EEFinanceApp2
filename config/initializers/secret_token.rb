9c1f55fd8b833177abd6eaca5f9449e5c04828ec
Matthew Gee <mattgee@gmail.com>
2013-05-05 00:03:59 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EEFinanceApp2::Application.config.secret_token = '7c9856276e413a469925c2feff20d4ee64dc5ef8a414639852954ee3055dc351808ddd5d21aacfa230c73bc38c3720ba292fb0f5ca0627bfc1959a4449b0cb63'
